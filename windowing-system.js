3d07623273b9709682c6a9ce7b93f9f6661ae567
mhmmd <mhmmd.rezaei4@gmail.com>
2023-03-09 17:55:04 +0330

    Class: Size

--- windowing-system.js
+++ windowing-system.js
@@ -1,1 +1,21 @@
 // i cant understand prototype and class, look back again and again read more ... 😑😶😐😔
+
+// const size = new Size(1080, 764);
+
+// console.log(size.width);
+// console.log(size.height);
+
+class Size {
+  constructor(width = 80, height = 60) {
+    this.height = height;
+    this.width = width;
+  }
+  resize(newWidth, newHeight) {
+    this.height = newHeight;
+    this.width = newWidth;
+  }
+}
+
+// size.resize(1920, 1080);
+// console.log(size.width);
+// console.log(size.height);
